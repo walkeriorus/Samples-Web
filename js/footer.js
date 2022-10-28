992ec36a73cfeb6cf5b6a161dc11dc49dd154e5f
televader <denispalacios@gmail.com>
2022-10-28 18:06:03 -0300

    Modificaciones en el index

---
+++ js/footer.js
@@ -0,0 +1,14 @@
+let cad=`
+    <a class="redsoc" href="https://www.twitter.com" target="_blank"><i class="fa fa-twitter"
+        aria-hidden="true"></i></a>
+    <a class="redsoc" href="https://www.facebook.com/marcela.cerda.9" target="_blank"><i class="fa fa-facebook"
+        aria-hidden="true"></i></a>
+    <a class="redsoc" href="https://www.pinterest" target="_blank"><i class="fa fa-pinterest"
+        aria-hidden="true "></i></a>
+    <a class="redsoc" href="https://www.instagram.com/marcela.ines.cerda/" target="_blank"><i class="fa fa-instagram"
+        aria-hidden="true"></i></a>
+    <a class="redsoc" href="https://www.linkedin.com/in/marcela-cerd%C3%A1-678b05196/" target="_blank"><i
+        class="fa fa-linkedin" aria-hidden="true"></i></a>
+    <p>Derechos reservados @2022 </p>
+`
+document.getElementById("idfooter").innerHTML=cad;
