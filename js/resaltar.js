d1ff553cbd2641b6e05f8eeee56ff51ad307f2c4
Walter Perez <walkeriorus472@gmail.com>
2022-11-02 15:45:23 -0300

    Eliminadas las lineas que imprimian valores por consola

--- js/resaltar.js
+++ js/resaltar.js
@@ -1,9 +1,6 @@
 let actualPath = window.location
 let pathName = actualPath.pathname
-console.log(actualPath)
-console.log( pathName )
 
 let navLinkAResaltar = document.querySelector(`.nav-link[href="${pathName}"]`);
-console.log(navLinkAResaltar);
 navLinkAResaltar.style.backgroundColor='white';
 navLinkAResaltar.style.color='black';
