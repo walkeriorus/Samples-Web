cddfb37a168be2180248b44c21aa3b01da6b628e
Walter Perez <walkeriorus472@gmail.com>
2022-11-04 13:56:30 -0300

    Añadido un segundo menu que se muestra en resoluciones de hasta 500px.Se añadió la etiqueta link para la hoja de estilos styles.css en todos los html.Modificaciones en resaltar.js, ahora la funcion se dispara en cada cambio de resolucion.

--- js/resaltar.js
+++ js/resaltar.js
@@ -1,6 +1,25 @@
 let actualPath = window.location
 let pathName = actualPath.pathname
-
-let navLinkAResaltar = document.querySelector(`.nav-link[href="${pathName}"]`);
-navLinkAResaltar.style.backgroundColor='white';
-navLinkAResaltar.style.color='black';
+/*Cual menu estoy mostrando?? */
+let identificarMenu = function () {
+    let menus = [...document.querySelectorAll(`div[id^=menu]`)].slice(0, 2);
+    let menu;
+    let navLinkAResaltar;
+    for (let pos = 0; pos < menus.length; pos++) {
+        if (window.getComputedStyle(menus[pos]).display === 'none') {
+            continue
+        }
+        else {
+            // console.log('Menu: ',menus[pos].id,'display: ', window.getComputedStyle(menus[pos]).display)
+            menu = menus[pos].id;
+            navLinkAResaltar = document.querySelector(`#${menu} .nav-link[href="${pathName}"]`);
+        }
+    }
+    navLinkAResaltar = document.querySelector(`#${menu} .nav-link[href="${pathName}"]`);
+    navLinkAResaltar.style.backgroundColor = 'white';
+    navLinkAResaltar.style.color = 'black';
+    // console.log('Ejecutando identificarMenu');
+    // console.log('Menu identificado: ',menu);
+}
+window.addEventListener('resize', identificarMenu);
+window.addEventListener('load', identificarMenu)
